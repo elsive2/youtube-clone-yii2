802883b42227917fc889ca62189eaffa8423797f
elsive2 <maximyakovlev228@gmail.com>
2022-09-20 22:01:18 +0600

    Can upload videos

---
+++ backend/views/video/create.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var common\models\Video $model */
+
+$this->title = 'Create Video';
+$this->params['breadcrumbs'][] = ['label' => 'Videos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="video-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="d-flex flex-column justify-content-center align-items-center">
+        <div class="upload-icon mb-4">
+            <i class="fas fa-upload"></i>
+        </div>
+
+        <p>Drag and drop a file you want to upload</p>
+        <p class="text-muted">Your video will be private until you publish it</p>
+
+        <?php \yii\bootstrap5\ActiveForm::begin(['options' => [
+            'enctype' => 'multipart/form-data',
+            'id' => 'video-file-form'
+        ]]) ?>
+
+        <button class="btn btn-primary btn-file">
+            Select file
+            <input type="file" id="video-file" name="video">
+        </button>
+
+        <?php \yii\bootstrap5\ActiveForm::end() ?>
+
+    </div>
+
+</div>
--- backend/web/js/app.js
+++ backend/web/js/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     $('#video-file').change(() => {
-        $(this).closest('form').trigger('submit');
+        $('#video-file-form').trigger('submit');
     })
 })
-
-alert(1);
